1841dc99e6d2be65ba8c50a54b2e775720995b91
Александр Михеичев <ya.saska20@gmail.com>
2022-12-08 00:02:53 +0300

    admin complete

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import (Ingredient, FavoritesRecipe, Recipe, ShoppingList,
+                            Subscription, Tag)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+    save_on_top = True
+
+
+@admin.register(FavoritesRecipe)
+class FavoritesRecipeAdmin(admin.ModelAdmin):
+    list_display = ('user.username', 'recipe.name',)
+    search_fields = ('user.username', 'recipe.name',)
+    list_filter = ('user.username', 'recipe.name',)
+    save_on_top = True
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('author.username', 'name', 'tags.name', 'cooking_time',)
+    search_fields = ('author.username', 'name', 'cooking_time',)
+    list_filter = ('author.username', 'name', 'tags.name', 'cooking_time',)
+    save_on_top = True
+
+    @staticmethod
+    @admin.display(description='Число рецептов в Избранном')
+    def get_favorites_recipe_count(obj):
+        return obj.in_favorites.count()
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('user.username', 'recipe.name',)
+    search_fields = ('user.username', 'recipe.name',)
+    list_filter = ('user.username', 'recipe.name',)
+    save_on_top = True
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user.username', 'subscribed_author.username',)
+    search_fields = ('user.username', 'subscribed_author.username',)
+    list_filter = ('user.username', 'subscribed_author.username',)
+    save_on_top = True
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug',)
+    search_fields = ('name', 'slug',)
+    list_filter = ('name', 'slug',)
+    save_on_top = True
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,12 +4,9 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import CustomUser
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'role',)
     list_filter = ('first_name', 'email',)
     search_fields = ('username', 'email',)
-    ordering = ('email',)
     save_on_top = True
-
-
-admin.site.register(CustomUser, CustomUserAdmin)

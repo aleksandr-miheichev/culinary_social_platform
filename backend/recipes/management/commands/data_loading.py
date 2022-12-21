8cc9ad2ff8dcb806aa3005c00fd20ee955f9042c
Александр Михеичев <ya.saska20@gmail.com>
2022-12-21 18:00:36 +0300

    fix management command

--- backend/recipes/management/commands/data_loading.py
+++ backend/recipes/management/commands/data_loading.py
@@ -1,43 +1,51 @@
 import csv
 
 from django.core.management import BaseCommand
-from reviews.models import (Category, Comment, CustomUser, Genre, GenreTitle,
-                            Review, Title)
 
-TABLES = {
-    CustomUser: 'users.csv',
-    Category: 'category.csv',
-    Genre: 'genre.csv',
-    Title: 'titles.csv',
-    GenreTitle: 'genre_title.csv',
-    Review: 'review.csv',
-    Comment: 'comments.csv',
-}
+from recipes.models import Ingredient
 
-replace_field = [
-    'author',
-    'category',
-]
+csv_file = 'ingredients.csv'
 
 
 class Command(BaseCommand):
-    help = "Loads data from csv files"
+    help = "Loading data from a csv file into the Ingredient model."
 
     def handle(self, *args, **kwargs):
-        for model, csv_file in TABLES.items():
-            file_path = f'./static/data/{csv_file}'
-            try:
-                f = open(file_path, 'r', encoding='utf8')
-            except FileNotFoundError:
-                print(f'Sorry, the file "{csv_file}" does not exist.')
-            else:
-                with f:
-                    reader = csv.DictReader(f, delimiter=',')
-                    for index, field in enumerate(reader.fieldnames):
-                        if field in replace_field:
-                            reader.fieldnames[index] += '_id'
-                    for data in reader:
-                        model.objects.get_or_create(**data)
         self.stdout.write(
-            self.style.SUCCESS('Database successfully loaded into models!')
+            self.style.WARNING(
+                'Started filling the Ingredient model from a csv file!'
+            )
+        )
+        file_path = f'./static/data/{csv_file}'
+        try:
+            f = open(file_path, 'r', encoding='utf-8')
+        except FileNotFoundError:
+            self.stdout.write(
+                self.style.ERROR(
+                    f'Sorry, the file "{csv_file}" does not exist!'
+                )
+            )
+        else:
+            fieldnames = ['name', 'measurement_unit', ]
+            with f:
+                reader = csv.DictReader(
+                    f,
+                    fieldnames=fieldnames,
+                    delimiter=','
+                )
+                for data in reader:
+                    obj, created = Ingredient.objects.get_or_create(
+                        name=data[fieldnames[0]],
+                        measurement_unit=data[fieldnames[1]],
+                    )
+                    if not created:
+                        self.stdout.write(
+                            self.style.ERROR(
+                                'An ingredient with the name: '
+                                f'"{data[fieldnames[0]]}" and the unit '
+                                f'"{data[fieldnames[1]]}" is already in the '
+                                'database!')
+                        )
+        self.stdout.write(
+            self.style.SUCCESS('Database successfully loaded into model!')
         )
